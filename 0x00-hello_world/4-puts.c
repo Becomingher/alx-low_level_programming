b7067961db7fa8c810c71b01640f136e7284aa08
Becomingher <scotiaprincewill16@gmail.com>
2023-04-20 19:45:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /*
  * main - A program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
  */
-int main (void)
+int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
-		return: 0;
+		return (0);
 }
