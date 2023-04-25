3da9dcaf5f303f1b8c8fd7bc6b1b61c0556e174c
Becomingher <scotiaprincewill16@gmail.com>
2023-04-25 16:21:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/*
+ * main -  a program that prints _putchar, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_");
+	printf("p");
+	printf("u");
+	printf("t");
+	printf("c");
+	printf("h");
+	printf("a");
+	printf("r\n");
+	return 0
+}
