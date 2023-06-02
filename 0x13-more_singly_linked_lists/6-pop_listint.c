1d2b873b8a9bb98fadbb18d7dc5cc8cb857a39e4
Becomingher <scotiaprincewill16@gmail.com>
2023-06-02 12:02:39 +0100

    ekelebe stunner

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* pop_listint - deletes head node of linked list
+* @head: pointer to the first element in the linked list
+* Return: ekelebe stunner
+*/
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int num;
+if (!head || !*head)
+return (0);
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (num);
+}
