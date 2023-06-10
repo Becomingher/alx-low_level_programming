99de18efce5218ffa1f5e0578dd1f926ef62ffb2
Becomingher <scotiaprincewill16@gmail.com>
2023-06-10 09:24:50 +0100

    God is a Woman.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* read_textfile- Read the text file print to STDOUT but tomorrow get belle.
+* @filename: text file being read
+* @letters: number of letters to be read
+* Return: why is it so lengthy?
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+char *buf;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
+free(buf);
+close(fd);
+return (w);
+}
